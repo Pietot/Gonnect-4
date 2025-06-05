de269251873ff1975d330d19681ea6e2d79f1980
Pietot <102545552+Pietot@users.noreply.github.com>
2025-06-06 00:11:47 +0200

    Updated main.go

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Pietot/Gonnect-4/grid"
 )
 
 func main() {
-	fmt.Println("Welcome to Gonnect 4!")
+	testGrid := grid.InitGrid()
+	testGrid.Grid = [][]int{
+		{0, 2, 2, 2, 1, 0, 0},
+		{0, 1, 1, 1, 2, 2, 2},
+		{0, 1, 2, 2, 1, 1, 1},
+		{1, 2, 1, 1, 2, 2, 2},
+		{2, 2, 1, 2, 1, 2, 1},
+		{1, 2, 1, 1, 2, 1, 1},
+	}
+	fmt.Println(testGrid.Negamax(2))
 }
