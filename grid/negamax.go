645a031771553ffe1e9769d560e67c71d5b49ad5
Pietot <102545552+Pietot@users.noreply.github.com>
2025-06-07 17:48:49 +0200

    Added stats to Negamax

--- grid/negamax.go
+++ grid/negamax.go
@@ -2,17 +2,46 @@ package grid
 
 import (
 	"math"
+	"time"
 
 	"github.com/Pietot/Gonnect-4/evaluation"
+	"github.com/Pietot/Gonnect-4/stats"
 	"github.com/Pietot/Gonnect-4/utils"
 )
 
-const COMPUTER = 1
-
 var stack = 0
 
-func (grid *Grid) Negamax(player int) *evaluation.Evaluation {
+func (grid *Grid) Negamax(player int) (*evaluation.Evaluation, *stats.Stats) {
+	start := time.Now()
+	nbPos := int64(0)
+
+	result := grid.negamaxStats(player, &nbPos)
+
+	elapsed := time.Since(start)
+	elapsedSeconds := elapsed.Seconds()
+	meanNbPos := float64(nbPos)
+	nbPosPerSec := 0.0
+	meanTimePerPos := 0.0
+	if meanNbPos > 0 {
+		meanTimePerPos = (elapsed.Seconds() * 1000) / meanNbPos
+	}
+	if elapsedSeconds > 0 {
+		nbPosPerSec = meanNbPos / elapsedSeconds
+	}
+
+	stats := &stats.Stats{
+		TotalTimeMs:         elapsed.Seconds() * 1000,
+		NumPositions:        nbPos,
+		MeanTimePerPosition: meanTimePerPos,
+		PositionsPerSecond:  nbPosPerSec,
+	}
+
+	return result, stats
+}
+
+func (grid *Grid) negamaxStats(player int, nbPos *int64) *evaluation.Evaluation {
 	if grid.IsDraw() {
+		*nbPos++
 		return &evaluation.Evaluation{
 			Score:         utils.Float64Ptr(0.0),
 			BestMove:      nil,
@@ -26,6 +55,7 @@ func (grid *Grid) Negamax(player int) *evaluation.Evaluation {
 		copyGrid := grid.DeepCopy()
 		droppedPiece, line := copyGrid.DropPiece(column, player)
 		if droppedPiece && copyGrid.CheckWinFromIndex(player, line, column) {
+			*nbPos++
 			return &evaluation.Evaluation{
 				Score:         utils.Float64Ptr(math.Inf(1)),
 				BestMove:      &column,
