f5c9cd2dd4d4cfb31311a5ee52bfbfabe9bea20e
Pietot <102545552+Pietot@users.noreply.github.com>
2025-07-31 23:13:43 +0200

    Change grid representation with bitboards

--- grid/grid.go
+++ grid/grid.go
@@ -1,142 +1,93 @@
 package grid
 
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	WIDTH  = 7
+	HEIGHT = 6
+)
+
 type Grid struct {
-	Grid    [][]int
-	nbMoves int
+	CurrentPosition uint64
+	Mask            uint64
+	nbMoves         int
 }
 
-func InitGrid() *Grid {
-	grid := &Grid{
-		Grid: make([][]int, 6),
-	}
-	for i := range grid.Grid {
-		grid.Grid[i] = make([]int, 7)
-	}
-	grid.nbMoves = 0
-	return grid
+func (grid *Grid) CanPlay(column int) bool {
+	return (grid.Mask & topMask(column)) == 0
 }
 
-func InitCustomGrid(custom_grid [][]int) *Grid {
-	return &Grid{
-		Grid:    custom_grid,
-		nbMoves: getNbMoves(custom_grid),
-	}
+func (grid *Grid) Play(column int) {
+	grid.CurrentPosition ^= grid.Mask
+	grid.Mask |= grid.Mask + bottomMask(column)
+	grid.nbMoves++
 }
 
-func (grid *Grid) String() string {
-	var output string
-	for _, row := range grid.Grid {
-		for _, cell := range row {
-			switch cell {
-			case 1:
-				// Color red for player 1
-				output += "\033[31m@\033[0m "
-			case 2:
-				// Color blue for player 2
-				output += "\033[34m@\033[0m "
-			default:
-				// Empty cell
-				output += "  "
-			}
-		}
-		output += "\n"
-	}
-	output += "-------------\n"
-	output += "1 2 3 4 5 6 7\n"
-	return output
-}
-
-func (grid *Grid) DropPiece(column int, player int) (bool, int) {
-	for i := len(grid.Grid) - 1; i >= 0; i-- {
-		if grid.Grid[i][column] == 0 {
-			grid.Grid[i][column] = player
-			grid.nbMoves++
-			return true, i
-		}
-	}
-	return false, -1
-}
-
-func (grid *Grid) CheckWinFromIndex(player int, line int, column int) bool {
-	// Horizontal check
-	count := 0
-	for index := range 7 {
-		if grid.Grid[line][index] == player {
-			count++
-			if count == 4 {
-				return true
-			}
-		} else {
-			count = 0
-		}
-	}
-	// Vertical check
-	count = 0
-	for index := range 6 {
-		if grid.Grid[index][column] == player {
-			count++
-			if count == 4 {
-				return true
-			}
-		} else {
-			count = 0
-		}
-	}
-	// Diagonal check
-	count = 0
-	for index := -3; index <= 3; index++ {
-		if line+index >= 0 && line+index < 6 && column+index >= 0 && column+index < 7 {
-			if grid.Grid[line+index][column+index] == player {
-				count++
-				if count == 4 {
-					return true
-				}
-			} else {
-				count = 0
-			}
-		}
-	}
-	// Anti-diagonal check
-	count = 0
-	for index := -3; index <= 3; index++ {
-		if line+index >= 0 && line+index < 6 && column-index >= 0 && column-index < 7 {
-			if grid.Grid[line+index][column-index] == player {
-				count++
-				if count == 4 {
-					return true
-				}
-			} else {
-				count = 0
-			}
-		}
-	}
-	return false
+func (grid *Grid) IsWinningMove(column int) bool {
+	position := grid.CurrentPosition
+	position |= (grid.Mask + bottomMask(column)) & columnMask(column)
+	return CheckWin(position)
 }
 
 func (grid *Grid) IsDraw() bool {
-	return grid.nbMoves == 6*7
+	return grid.nbMoves == WIDTH*HEIGHT
 }
 
-func (grid *Grid) DeepCopy() *Grid {
-	newGrid := make([][]int, len(grid.Grid))
-	for i := range grid.Grid {
-		newGrid[i] = make([]int, len(grid.Grid[i]))
-		copy(newGrid[i], grid.Grid[i])
-	}
-	return &Grid{
-		Grid:    newGrid,
-		nbMoves: grid.nbMoves,
-	}
+func (grid *Grid) key() uint64 {
+	return grid.CurrentPosition + grid.Mask
 }
 
-func getNbMoves(custom_grid [][]int) int {
-	count := 0
-	for _, row := range custom_grid {
-		for _, cell := range row {
-			if cell != 0 {
-				count++
-			}
+func InitGrid(columnsSequence string) (*Grid, error) {
+	grid := &Grid{}
+	for _, columnRune := range columnsSequence {
+		column, err := strconv.Atoi(string(columnRune))
+		if err != nil {
+			return grid, fmt.Errorf("invalid column character: %v", err)
 		}
+		column -= 1
+		if column < 0 || column >= WIDTH || !grid.CanPlay(column) || grid.IsWinningMove(column) {
+			return grid, fmt.Errorf("can't play at column %d", column+1)
+		}
+		grid.Play(column)
 	}
-	return count
+	return grid, nil
+}
+
+func CheckWin(position uint64) bool {
+	// Horizontal
+	mask := position & (position >> (HEIGHT + 1))
+	if mask&(mask>>(2*(HEIGHT+1))) != 0 {
+		return true
+	}
+
+	// Vertical
+	mask = position & (position >> 1)
+	if mask&(mask>>2) != 0 {
+		return true
+	}
+
+	// Diagonal 1 (\)
+	mask = position & (position >> HEIGHT)
+	if mask&(mask>>(2*HEIGHT)) != 0 {
+		return true
+	}
+
+	// Anti-Diagonal (/)
+	mask = position & (position >> (HEIGHT + 2))
+	return mask&(mask>>(2*(HEIGHT+2))) != 0
+}
+
+func topMask(column int) uint64 {
+	return (uint64(1) << (HEIGHT - 1)) << (column * (HEIGHT + 1))
+}
+
+func bottomMask(column int) uint64 {
+	return uint64(1) << (column * (HEIGHT + 1))
+}
+
+func columnMask(column int) uint64 {
+	return ((uint64(1) << HEIGHT) - 1) << (column * (HEIGHT + 1))
 }
