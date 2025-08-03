0ab4b51060e28b10e55983c2c53760fd90bc55cc
Pietot <102545552+Pietot@users.noreply.github.com>
2025-08-03 17:38:34 +0200

    Added transposition_table.go

---
+++ transposition_table/transposition_table.go
@@ -0,0 +1,44 @@
+package transposition_table
+
+type TranspositionTable struct {
+	table []Entry
+}
+
+type Entry struct {
+	key_56_bit uint64
+	value      uint8
+}
+
+func (trans_table *TranspositionTable) Init() {
+	trans_table.table = make([]Entry, 1610612741)
+}
+
+func (trans_table *TranspositionTable) Reset() {
+	for i := range trans_table.table {
+		trans_table.table[i] = Entry{}
+	}
+}
+
+func (trans_table *TranspositionTable) Put(key uint64, value uint8) {
+	if key >= (1 << 56) {
+		panic("Key out of range (must be < 2^56)")
+	}
+	entryIndex := trans_table.index(key)
+	trans_table.table[entryIndex].key_56_bit = key
+	trans_table.table[entryIndex].value = value
+}
+
+func (trans_table *TranspositionTable) Get(key uint64) uint8 {
+	if key >= (1 << 56) {
+		panic("Key out of range (must be < 2^56)")
+	}
+	entryIndex := trans_table.index(key)
+	if trans_table.table[entryIndex].key_56_bit == key {
+		return trans_table.table[entryIndex].value
+	}
+	return 0
+}
+
+func (trans_table *TranspositionTable) index(key uint64) uint64 {
+	return key % uint64(len(trans_table.table))
+}
