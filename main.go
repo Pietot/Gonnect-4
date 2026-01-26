5dbb3542ee7edf535b974d22fda532ceeb4f4610
Pietot <102545552+Pietot@users.noreply.github.com>
2026-01-26 12:19:53 +0100

    feat(main): update argument parsing and add --disable-book flag

--- main.go
+++ main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
+	"github.com/Pietot/Gonnect-4/config"
 	"github.com/Pietot/Gonnect-4/grid"
 )
 
 func printUsage() {
+	fmt.Println()
 	fmt.Println("Usage:")
-	fmt.Println("")
-	fmt.Println("    --a | --analyse [sequence]")
-	fmt.Println("")
-	fmt.Println("\033[35m        Analyze a position by giving a score for all possible moves\033[0m")
-	fmt.Println("")
-	fmt.Println("    --s | --solve   [sequence]")
-	fmt.Println("")
-	fmt.Println("\033[35m        Solve a position by giving its score and remaining moves\033[0m")
-	fmt.Println("")
+	fmt.Println()
+	fmt.Println("    -a")
+	fmt.Println("\033[35m    Analyze a position by giving a score for all possible moves\033[0m")
+	fmt.Println()
+	fmt.Println("    -s")
+	fmt.Println("\033[35m    Solve a position by giving its score and remaining moves\033[0m")
+	fmt.Println()
+	fmt.Println("    --disable-book")
+	fmt.Println("\033[35m    Disable the opening book for analyzing/solving positions\033[0m")
+	fmt.Println()
+	fmt.Println("    <sequence>")
+	fmt.Println("\033[35m    A sequence of digits representing the moves played so far.\n    Columns are numbered from 1 to 7.\033[0m")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println()
+	fmt.Println("\033[32m    ./gonnect4 -s")
+	fmt.Println("    ./gonnect4 -a 32164625")
+	fmt.Println("    ./gonnect4 -s --disable-book 5654767662\033[0m")
+	fmt.Println()
 	os.Exit(1)
 }
 
@@ -31,34 +44,32 @@ func parseNumbers(arg string) (string, error) {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("\033[31mError: insufficient arguments\033[0m")
-		printUsage()
-	} else if len(os.Args) > 3 {
-		fmt.Println("\033[31mError: too many arguments\033[0m")
+	// Don't forget to import the book package.
+	// Uncomment the following line to continue/recreate building the book.
+	// book.CreateBook(42)
+
+	flag.Usage = printUsage
+
+	analyze := flag.Bool("a", false, "Analyze a position")
+	solve := flag.Bool("s", false, "Solve a position")
+	disableBookFlag := flag.Bool("disable-book", false, "Disable opening book")
+	config.IsBookEnabled = !*disableBookFlag
+
+	flag.Parse()
+
+	if (*analyze && *solve) || (!*analyze && !*solve) {
+		fmt.Println("\033[31mError: you must provide exactly one of -a or -s\033[0m")
 		printUsage()
 	}
 
-	command := os.Args[1]
-	args := []string{""}
-	if len(os.Args) > 2 {
-		args = os.Args[2:]
-	}
-
-	isAnalyze := command == "--a" || command == "--analyse"
-	isSolve := command == "--s" || command == "--solve"
-
-	if !isAnalyze && !isSolve {
-		fmt.Printf("\033[31mError: unknown command '%s'\033[0m\n", command)
+	args := flag.Args()
+	if len(args) > 1 {
+		fmt.Println("\033[31mError: too many sequences\033[0m")
 		printUsage()
 	}
-	if len(os.Args) > 2 {
-		for _, a := range os.Args[2:] {
-			if a == "--a" || a == "--analyse" || a == "--s" || a == "--solve" {
-				fmt.Println("\033[31mError: multiple commands provided\033[0m")
-				printUsage()
-			}
-		}
+
+	if len(args) == 0 {
+		args = append(args, "")
 	}
 
 	sequence, err := parseNumbers(args[0])
@@ -73,11 +84,12 @@ func main() {
 		printUsage()
 	}
 
-	if isAnalyze {
-		fmt.Println(grid.Analyze())
+	if *analyze {
+		evaluation, stats := grid.Analyze()
+		fmt.Printf("Evaluation:\n%s\n\nStats: \n%s\n", evaluation, stats)
 	}
 
-	if isSolve {
+	if *solve {
 		evaluation, stats := grid.Solve()
 		fmt.Printf("Evaluation:\n%s\n\nStats: \n%s\n", evaluation, stats)
 	}
