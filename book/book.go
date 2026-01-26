25413463a989a8c8c46acde6b6993044f9f574d7
Pietot <102545552+Pietot@users.noreply.github.com>
2026-01-26 12:13:26 +0100

    feat(book): implement CreateBook function to precompute position that takes more than 20M nodes

---
+++ book/book.go
@@ -0,0 +1,84 @@
+package book
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Pietot/Gonnect-4/database"
+	"github.com/Pietot/Gonnect-4/grid"
+	"go.etcd.io/bbolt"
+)
+
+const NODE_THRESHOLD = 20_000_000
+
+func CreateBook(maxDepth int) {
+	db, err := bbolt.Open(database.DBName, 0600, &bbolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatalf("Critical error: cannot open database."+
+			"Check if another instance is running: %v", err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bbolt.Tx) error {
+		tx.CreateBucketIfNotExists([]byte(database.BucketResults))
+		tx.CreateBucketIfNotExists([]byte(database.BucketQueue))
+		tx.CreateBucketIfNotExists([]byte(database.BucketPending))
+		return nil
+	})
+
+	var isEmpty bool
+	db.View(func(tx *bbolt.Tx) error {
+		isEmpty = tx.Bucket([]byte(database.BucketQueue)).Stats().KeyN == 0
+		return nil
+	})
+
+	if isEmpty {
+		startGrid, _ := grid.InitGrid("")
+		db.Update(func(tx *bbolt.Tx) error {
+			database.AddToQueue(tx, grid.GetCanonicalKey(startGrid), 0)
+			return nil
+		})
+	}
+
+	for {
+		key, depth, found := database.PopFromQueue(db)
+		if !found || depth > maxDepth {
+			fmt.Println("End of calculation.")
+			break
+		}
+
+		g := grid.FromKey(key)
+
+		var alreadyDone bool
+		db.View(func(tx *bbolt.Tx) error {
+			alreadyDone = database.IsAnalyzed(tx, key)
+			return nil
+		})
+
+		if alreadyDone {
+			continue
+		}
+
+		analysis, stats := g.Analyze()
+		db.Update(func(tx *bbolt.Tx) error {
+			if stats.NodeCount >= NODE_THRESHOLD {
+				database.SaveResult(tx, key, analysis.Scores)
+				fmt.Printf("[D:%d] %d saved (%d nodes)\n", depth, key, stats.NodeCount)
+			}
+
+			for col := range 7 {
+				if g.CanPlay(col) {
+					child := *g
+					child.PlayColumn(col)
+					cKey := grid.GetCanonicalKey(&child)
+
+					if !database.IsAnalyzed(tx, cKey) && !database.IsInQueue(tx, cKey) {
+						database.AddToQueue(tx, cKey, depth+1)
+					}
+				}
+			}
+			return nil
+		})
+	}
+}
